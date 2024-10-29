1fd459f553628b43dd732004766884cac70b5796
MattVogelsang <Vogelsangmatt@gmail.com>
2024-10-29 12:11:45 -0400

    added index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,19 @@
-\c employees_db
+\c employees_db;
 
 INSERT INTO department(name)
 VALUES ('Sales'),
-        ('Engineering');
+       ('Engineering');
 
-    INSERT INTO role (title,salary,department_id)
-    VALUES('Sale Lead',100000,1),   
-          ('Sale Person',80000,1),
-          ('Lead Engineering',150000,2);
+INSERT INTO role(title,salary,department_id)
+VALUES ('Sales Lead',100000,1),
+       ('Sales Person',80000,1),
+       ('Lead Engineer',1500000,2);
 
-          INSERT INTO employee(first_name,last_name, role_id )
-          VALUES('John','Doe',1),
-                ('Mike','Chan',2),
-                ('Ashley','Rodriguez',3);
+INSERT INTO employee(first_name, last_name, role_id )
+VALUES('John', 'Doe',1),
+      ('Mike', 'Chan',2),
+      ('Ashley', 'Rodriguez',3);
 
-                UPDATE employee SET manager_id=1 WHERE id=2;
+
+ UPDATE  employee SET manager_id =1  WHERE id=2;
+    
